ed0eb1512e5de2505c7c1b911d3ba1a0c88649fb
geagl-ts <miguel.toledo@itistmo.edu.mx>
2020-04-07 00:07:06 -0500

    :implementacion:la tabla esta lista

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Inicio de la pagina</h1>
-    </div>
-  );
+  const [carreras, setCarreras] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getCarreras = async () => {
+    const response = await fetch(
+      "https://crud-carreras-api.herokuapp.com/api/v1/carreras",
+      {
+        mode: "no-cors",
+      }
+    );
+
+    console.log(response);
+
+    //setCarreras(carreras);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCarreras();
+  }, []);
+
+  if (loading) {
+    return <div className="container"></div>;
+  } else {
+    return (
+      <div className="container">
+        <div className="content">
+          <table className="table">
+            <thead>
+              <th>ID</th>
+              <th>Nombre</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Ing. Sistemas</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
